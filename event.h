140cde9a601523a71c3a0c1227367374ac88847c
skuzzis <andreieurt@gmail.com>
2024-12-08 19:13:56 +0000

    feat(src): Events

---
+++ event.h
@@ -0,0 +1,37 @@
+#ifndef _ext_event_h
+#define _ext_event_h
+
+#include "core.h"
+#include <any>
+#include <vector>
+#include <string>
+#include "eventresult.h"
+
+typedef int (*TrigEvent)(const char*, const char*, void*, void*);
+
+void* trigPtr = nullptr;
+
+EventResult TriggerEvent(std::string extension_id, std::string event, std::vector<std::any> args, std::any& eventReturn)
+{
+    if(!trigPtr) {
+        HINSTANCE m_hModule;
+        #ifdef _WIN32
+            m_hModule = dlmount("addons/swiftly/bin/win64/swiftly.dll");
+        #else
+            m_hModule = dlopen("addons/swiftly/bin/linuxsteamrt64/swiftly.so", RTLD_NOW);
+            if(!m_hModule) return EventResult::Continue;
+        #endif
+
+        void* trigPtr = reinterpret_cast<void*>(dlsym(m_hModule, "swiftly_TriggerEvent"));
+        if(!trigPtr) {
+            dlclose(m_hModule);
+            return EventResult::Continue;
+        }
+
+        dlclose(m_hModule);
+    }
+
+    return reinterpret_cast<TrigEvent>(trigPtr)(extension_id.c_str(), event.c_str(), (void*)&args, (void*)&eventReturn);
+}
+
+#endif
