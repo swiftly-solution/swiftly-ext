0b2cae2b6519026912cad2356dfa39ed173dc6de
skuzzis <andreieurt@gmail.com>
2024-12-08 15:03:49 +0000

    feat(core): Save, Declare, Create Global Vars

--- core.h
+++ core.h
@@ -16,4 +16,16 @@ EXT_API void* GetExtensionClass();
 #define EXT_EXPOSE(var)	\
 	EXT_API void* GetExtensionClass() { return reinterpret_cast<void*>(&var); }
 
+#define SAVE_GLOBALVARS() \
+	g_SHPtr = SHPtr;\
+	g_SMAPI = ismm
+
+#define DECLARE_GLOBALVARS() \
+	extern SourceHook::ISourceHook *g_SHPtr; \
+	extern ISmmAPI* g_SMAPI
+
+#define CREATE_GLOBALVARS() \
+	SourceHook::ISourceHook *g_SHPtr = nullptr; \
+	ISmmAPI* g_SMAPI = nullptr
+
 #endif
