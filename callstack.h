2632a34106f588510024555d3d85c433ea5f3dc5
skuzzis <andreieurt@gmail.com>
2024-12-09 11:53:20 +0000

    update(callstack): idek what i'm doing wrong

--- callstack.h
+++ callstack.h
@@ -1,12 +1,11 @@
 #ifndef _callstack_h
 #define _callstack_h
 
+#include <string>
 #include <metamod_oslink.h>
 #ifndef _WIN32
 #include <dlfcn.h>
 #endif
-
-#include <string>
 
 #define REGISTER_CALLSTACK(plugin_name, stack_str) RegisterCallstack DUIHRSGUI8RHG789342VGH78IU34FIUWEF(plugin_name, stack_str)
 
