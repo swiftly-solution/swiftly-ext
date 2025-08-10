ae1f37cfcfe5c0676b4140dca7290c4a44fccf19
skuzzis <andreieurt@gmail.com>
2025-08-10 09:25:03 +0000

    fix(embedder): Call the function

--- embedder.cpp
+++ embedder.cpp
@@ -6,13 +6,14 @@
 #include <dlfcn.h>
 #endif
 
+typedef void* (*here)(int);
+
 void* GetDotnetPointer(int kind)
 {
     void* ptr;
     HINSTANCE m_hModule;
 #ifdef _WIN32
     m_hModule = dlmount(GeneratePath("addons/swiftly/bin/win64/swiftly.dll"));
-    if (!m_hModule) return nullptr;
 #else
     m_hModule = dlopen(GeneratePath("addons/swiftly/bin/linuxsteamrt64/swiftly.so"), RTLD_NOW);
     if (!m_hModule) return nullptr;
@@ -20,5 +21,7 @@ void* GetDotnetPointer(int kind)
 
     ptr = reinterpret_cast<void*>(dlsym(m_hModule, "swiftly_GetDotnetPointer"));
     dlclose(m_hModule);
-    return ptr;
+
+    if (ptr) return reinterpret_cast<here>(ptr)(kind);
+    else return nullptr;
 }
